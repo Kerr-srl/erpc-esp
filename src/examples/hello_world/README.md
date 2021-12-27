a13711bb3a4607e0adec6312af0b551c8ce5da44
Lijun Chen <chenlijun1999@gmail.com>
2021-12-27 14:43:50 +0100

    docs: improve hello_world example
    * Support communication between two ESP32
    * Communication between Python and ESP32 should happen on another UART
      port (not the one used for logging and flashing)
    
    Should solve #2.

--- src/examples/hello_world/README.md
+++ src/examples/hello_world/README.md
@@ -4,20 +4,46 @@
 
 Check the documentation of the [erpc](../../components/erpc/README.md) component.
 
+### Communication between ESP32 and PC
+
 ```bash
 # Build and flash the firmware on the ESP32 target
 idf.py build
 idf.py flash
 
-# Communicate with the target via UART (e.g. at /dev/ttyUSB0) using the host-side Python script
-python main/main.py -p /dev/ttyUSB0
+# Connect the other UART of the board with the PC through an USB-UART bridge.
+# Let's say the USB-UART bridge is connected on port /dev/ttyACM0
+# We can communicate with the target using the host-side Python script
+python main/main.py -p /dev/ttyACM0
+```
+
+### Communication between two ESP32, one as host and one as target
+
+Assuming that:
+
+* the "host ESP32" is connected to the PC at port /dev/ttyUSB0
+* the "target ESP32" is connected to the PC at port /dev/ttyUSB1
+* "host ESP32" and "target ESP32" are connected via the other UART (the one that is not used for flashing and logging)
+
+```bash
+# Build and flash the "host" firmware
+idf.py -Bbuild/host build -DHOST=TRUE
+ESPPORT=/dev/ttyUSB0 idf.py -Bbuild/host flash
+
+# Build and flash the "target" firmware
+idf.py -Bbuild/target build -DHOST=FALSE
+ESPPORT=/dev/ttyUSB1 idf.py -Bbuild/target flash
+
+# Watch them communicate with each other
+ESPPORT=/dev/ttyUSB0 idf.py -Bbuild/host monitor
+ESPPORT=/dev/ttyUSB1 idf.py -Bbuild/target monitor
 ```
 
 ## Problem of connectionless UART communication
 
-Reset the ESP32, start the Python script after 6 seconds. You'll see that the Python script (the host) successfully calls the ESP32 (the target) and gets a reply back, while the calls that the ESP32 is supposed to make every five seconds don't happen. This is because the first call from the ESP32 was lost since the Python script was not up running yet.
+Reset the ESP32, start the Python script after 2 seconds. You'll see that the Python script (the host) successfully calls the ESP32 (the target) and gets a reply back, while the calls that the ESP32 is supposed to make every second don't happen. This is because the first call from the ESP32 was lost since the Python script was not up running yet.
 
-Fundamentally, we need a sort of handshake mechanism, we need to build a connection before communicating.
+Fundamentally, we need a sort of handshake mechanism, we need to establish a connection before communicating.
 
 Considerations:
 
--- src/examples/hello_world/main/main.c
+++ src/examples/hello_world/main/main.c
@@ -5,18 +5,37 @@
 
 #include "erpc_esp_uart_transport_setup.h"
 
-#include "gen/hello_world_host.h"
-#include "gen/hello_world_target_server.h"
+#if HOST
+#	include "gen/hello_world_host_server.h"
+#	include "gen/hello_world_target.h"
+#else
+#	include "gen/hello_world_host.h"
+#	include "gen/hello_world_target_server.h"
+#endif
 
 #include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #define TAG "main"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/gpio.h"
 #include "sdkconfig.h"
 
 #include <string.h>
 
+#if HOST
+char *say_hello_to_host(const char *name, uint32_t ith) {
+	size_t len = strlen("Hello ") + strlen(name) + 1;
+	char *response = erpc_malloc(len);
+	memset(response, 0, len);
+	strcat(response, "Hello ");
+	strcat(response, name);
+	ESP_LOGI(TAG, "Received call from target. Responding with \"%s\"",
+			 response);
+	return response;
+}
+#else
 char *say_hello_to_target(const char *name, uint32_t ith) {
 	size_t len = strlen("Hello ") + strlen(name) + 1;
 	char *response = erpc_malloc(len);
@@ -26,6 +45,7 @@ char *say_hello_to_target(const char *name, uint32_t ith) {
 	ESP_LOGI(TAG, "Received call from host. Responding with \"%s\"", response);
 	return response;
 }
+#endif
 
 void server_task(void *params) {
 	ESP_LOGI(TAG, "Starting server");
@@ -37,26 +57,42 @@ void server_task(void *params) {
 void client_task(void *params) {
 	uint32_t i = 0;
 	while (1) {
-		vTaskDelay(pdMS_TO_TICKS(5000));
+		vTaskDelay(pdMS_TO_TICKS(1000));
+#if HOST
+		ESP_LOGI(TAG, "Calling target");
+		char *response = say_hello_to_target("ESP32 host", i);
+		ESP_LOGI(TAG, "Target response: \"%s\"", response);
+#else
 		ESP_LOGI(TAG, "Calling host");
-		char *response = say_hello_to_host("ESP32", i);
+		char *response = say_hello_to_host("ESP32 target", i);
 		ESP_LOGI(TAG, "Host response: \"%s\"", response);
+#endif
 		++i;
 	}
 	vTaskDelete(NULL);
 }
 
+#define RPC_UART_PORT ((CONFIG_ESP_CONSOLE_UART_NUM + 1) % UART_NUM_MAX)
+// Change these pins as needed to make them work on your hardware
+#define RPC_UART_TX GPIO_NUM_19
+#define RPC_UART_RX GPIO_NUM_20
+
 void app_main() {
 	ESP_LOGD(TAG, "Initializing UART transport");
-	erpc_transport_t transport =
-		erpc_esp_transport_uart_init(CONFIG_ESP_CONSOLE_UART_NUM);
+	erpc_transport_t transport = erpc_esp_transport_uart_init(RPC_UART_PORT);
+	ESP_ERROR_CHECK(uart_set_pin(RPC_UART_PORT, RPC_UART_TX, RPC_UART_RX,
+								 UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 	erpc_mbf_t message_buffer_factory = erpc_mbf_static_init();
 	erpc_transport_t arbitrator =
 		erpc_arbitrated_client_init(transport, message_buffer_factory);
 
 	ESP_LOGD(TAG, "Initializing server");
 	erpc_server_init(arbitrator, message_buffer_factory);
+#if HOST
+	erpc_add_service_to_server(create_hello_world_host_service());
+#else
 	erpc_add_service_to_server(create_hello_world_target_service());
+#endif
 
 	xTaskCreate(server_task, "server", 0x1000, NULL, 2, NULL);
 	xTaskCreate(client_task, "client", 0x1000, NULL, 2, NULL);
