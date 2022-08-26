9975fac8dcd2cbb036f6bdf88810d5ad8d097766
Lijun Chen <chenlijun1999@gmail.com>
2022-08-26 22:38:22 +0200

    fix(log): return FreeRTOS milliseconds instead of value from CLOCK_MONOTONIC

--- src/examples/host/components/log/log_linux.c
+++ src/examples/host/components/log/log_linux.c
@@ -27,10 +27,6 @@ void esp_log_impl_unlock(void) {
 }
 
 uint32_t esp_log_timestamp(void) {
-	struct timespec current_time;
-	int result = clock_gettime(CLOCK_MONOTONIC, &current_time);
-	assert(result == 0);
-	uint32_t milliseconds =
-		current_time.tv_sec * 1000 + current_time.tv_nsec / 1000000;
-	return milliseconds;
+	TickType_t tick_count = xTaskGetTickCount();
+	return tick_count * (1000 / configTICK_RATE_HZ);
 }
